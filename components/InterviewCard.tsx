db1f77f39adf0fd981da14d2db7eb38ef4db7ffe
kshitijslays <rajkshitij876@gmail.com>
2025-12-19 12:49:15 +0530

    implement interview session

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,12 +1,12 @@
 import dayjs from "dayjs";
 import Image from "next/image";
-import Link from 'next/link'
-import {Button} from "@/components/ui/button"
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import { cn, getRandomInterviewCover } from "@/lib/utils";
 import DisplayTechIcons from "./DisplayTechIcons";
 
 const InterviewCard = ({
-  interviewId,
+  id,
   userId,
   role,
   type,
@@ -35,31 +35,35 @@ const InterviewCard = ({
           <h3 className="mt-5 capitalize">{role} Interview</h3>
           <div className="flex flex-row gap-5 mt-3">
             <div className="flex flex-row gap-2">
-                <Image src="/calendar.svg" alt="calendar" width={22} height={22} />
-                <p>{formattedDate}</p>
+              <Image
+                src="/calendar.svg"
+                alt="calendar"
+                width={22}
+                height={22}
+              />
+              <p>{formattedDate}</p>
             </div>
             <div className="flex flex-row gap-2 items-center">
-                <Image src="/star.svg" alt="star" width={22} height={22} />
-                <p>{feedback?.totalScore || '---'}/100</p>
+              <Image src="/star.svg" alt="star" width={22} height={22} />
+              <p>{feedback?.totalScore || "---"}/100</p>
             </div>
           </div>
           <p className="line-clamp-2 mt-5">
-            {feedback?.finalAssessment || "You haven't taken the interview yet. Take it now to improve your skills."}
+            {feedback?.finalAssessment ||
+              "You haven't taken the interview yet. Take it now to improve your skills."}
           </p>
         </div>
 
         <div className="flex flex-row justify-between">
-            <DisplayTechIcons techStack = {techstack} />
-            <Button className="btn-primary">
-                <Link href={feedback
-                  ? '/interview/${interviewId}/feedback'
-                  : '/interview/${interviewId}'
-                }>
-                  {feedback? 'Check Feedback': 'View Interview'}
-                </Link>
-            </Button>
+          <DisplayTechIcons techStack={techstack} />
+          <Button className="btn-primary">
+            <Link
+              href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}
+            >
+              {feedback ? "Check Feedback" : "View Interview"}
+            </Link>
+          </Button>
         </div>
-
       </div>
     </div>
   );
